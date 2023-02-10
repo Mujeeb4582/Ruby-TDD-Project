6e7acfd11ab230637f94cdd4fc0107fbb8ca70e5
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-10 15:55:20 +0500

    Add the test for checking factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,17 @@
 require './lib/solver'
 
 describe Solver do
-  let(:solver) { Solver.new(10) }
+  let(:solver) { Solver.new(4) }
 
   describe '#initialize' do
     it 'sets the given number' do
-      expect(solver.number).to eq(10)
+      expect(solver.number).to eq(4)
+    end
+  end
+
+  describe '#factorial' do
+    it 'returns the factorial of the given number' do
+      expect(solver.factorial).to eq(24)
     end
   end
 end
