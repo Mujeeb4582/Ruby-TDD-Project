e183d50ef110564501f06217e98f4df9bcf4c968
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-18 22:52:14 +0500

    Add the test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,6 +2,7 @@ require './lib/solver'
 
 describe Solver do
   solver = Solver.new
+
   describe '#factorial' do
     it 'raises an error if the given number is negative' do
       expect { solver.factorial(-1) }.to raise_error('Negative numbers not allowed')
@@ -23,6 +24,24 @@ describe Solver do
   describe '#reverse' do
     it 'returns the inverse of of the given string' do
       expect(solver.reverse_string('word')).to eq 'drow'
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it "returns 'buzz' when number divisible by 15" do
+      expect(solver.fizzbuzz(45)).to eq 'fizzbuzz'
+    end
+
+    it "returns 'fizz' when number divisible by 3" do
+      expect(solver.fizzbuzz(9)).to eq 'fizz'
+    end
+
+    it "returns 'buzz' when number divisible by 5" do
+      expect(solver.fizzbuzz(25)).to eq 'buzz'
+    end
+
+    it 'returns given number if number not divisible by 15 or 5 or 3' do
+      expect(solver.fizzbuzz(14)).to eq 14
     end
   end
 end
