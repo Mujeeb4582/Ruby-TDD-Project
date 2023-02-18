a80df490a72c9f6ea86b26a3c8aad3e34945ef3d
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-18 22:33:09 +0500

    Add the test for reverseString

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,26 +1,28 @@
 require './lib/solver'
 
 describe Solver do
-
+  solver = Solver.new
   describe '#factorial' do
     it 'raises an error if the given number is negative' do
-      solver = Solver.new
-      expect{(solver.factorial(-1))}.to raise_error('Negative numbers not allowed')
+      expect { solver.factorial(-1) }.to raise_error('Negative numbers not allowed')
     end
 
     it 'returns the factorial of the given number' do
-      solver = Solver.new
       expect(solver.factorial(4)).to eq 24
     end
 
     it 'returns the factorial of the given number' do
-      solver = Solver.new
       expect(solver.factorial(1)).to eq 1
     end
 
     it 'returns the factorial of the given number' do
-      solver = Solver.new
       expect(solver.factorial(0)).to eq 1
+    end
+  end
+
+  describe '#reverse' do
+    it 'returns the inverse of of the given string' do
+      expect(solver.reverse_string('word')).to eq 'drow'
     end
   end
 end
