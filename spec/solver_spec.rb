b351f6632f18faadaa2e42f04d60ca91c03efd71
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-18 22:23:31 +0500

    Add the test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,17 +1,26 @@
 require './lib/solver'
 
 describe Solver do
-  let(:solver) { Solver.new(4) }
-
-  describe '#initialize' do
-    it 'sets the given number' do
-      expect(solver.number).to eq(4)
-    end
-  end
 
   describe '#factorial' do
+    it 'raises an error if the given number is negative' do
+      solver = Solver.new
+      expect{(solver.factorial(-1))}.to raise_error('Negative numbers not allowed')
+    end
+
     it 'returns the factorial of the given number' do
-      expect(solver.factorial).to eq(24)
+      solver = Solver.new
+      expect(solver.factorial(4)).to eq 24
+    end
+
+    it 'returns the factorial of the given number' do
+      solver = Solver.new
+      expect(solver.factorial(1)).to eq 1
+    end
+
+    it 'returns the factorial of the given number' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq 1
     end
   end
 end
