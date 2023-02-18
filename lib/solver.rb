05c6fba3cb2cc77f2ed5d4247d0f5c0f80add7f8
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-18 22:23:06 +0500

    Add the factorial finding method

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,6 +1,8 @@
 class Solver
-  attr_accessor :number
-  def initialize(number)
-    @number = number
+
+  def factorial(number)
+    raise "Negative numbers not allowed" if number < 0
+    return 1 if number == 0 || number == 1
+    return number * factorial(number-1)
   end
 end
